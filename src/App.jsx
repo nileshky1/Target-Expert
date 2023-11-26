ce384b81cd2dda5baa19ac9d634f166cb096ff6c
Nilesh Yadav <nileshky7@gmail.com>
2023-11-26 22:59:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import InputField from "./components/InputField";
+import Problem from "./components/Problem";
+import data from "./finalData.json";
+
+import { useState } from "react";
+
+function App() {
+  const [userName, setUserName] = useState("");
+  const [userSolved, setUserSolved] = useState([]);
+
+  return (
+    <>
+      <NavBar />
+      <InputField
+        userName={userName}
+        setUserName={setUserName}
+        userSolved={userSolved}
+        setUserSolved={setUserSolved}
+      />
+      <div className="flex flex-wrap justify-center">
+        {Object.entries(data).map(([key, value]) => {
+          return <Problem data={value} userSolved={userSolved} />;
+        })}
+      </div>
+      <div>
+        <p className="flex justify-center text-lg m-6">
+          Made with ❤ by &nbsp;
+          <a href="https://codeforces.com/profile/coder_ravan" target="_blank">Coder_Ravan</a>{" "}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/InputField.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function InputField(props) {
+  const [solved, setSolved] = useState(false);
+
+  function createURL(conestId, problemIndex) {
+    return `https://codeforces.com/problemset/problem/${conestId}/${problemIndex}`;
+  }
+
+  const updateData = () => {
+    props.setUserSolved([]);
+    axios
+      .get(`https://codeforces.com/api/user.status?handle=${props.userName}`)
+      .then((res) => {
+        let data = res.data.result;
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].verdict === "OK") {
+            let problem = data[i].problem;
+            let url = createURL(problem.contestId, problem.index);
+            props.setUserSolved((prev) => {
+              return [...prev, url];
+            });
+          }
+        }
+      });
+    // console.log(props.userSolved);
+    console.log(props.userName);
+  };
+
+  useEffect(() => {
+    props.setUserName("");
+  }, []);
+
+  return (
+    <div className="flex justify-center m-10 ">
+      <input
+        type="text"
+        placeholder="Enter the UserName"
+        className="input input-bordered input-accent w-full max-w-xs mx-12"
+        onChange={(e) => props.setUserName(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") {
+            updateData();
+          }
+        }}
+      />
+      <button className="btn btn-outline btn-success" onClick={updateData}>
+        Let's Go 🚀🚀
+      </button>
+    </div>
+  );
+}
+
+export default InputField;
